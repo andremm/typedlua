146e73eeb8d68845b06e7fdbb16b708a46dac119
Andre Murbach Maidl <andremm@gmail.com>
2013-05-27 23:46:13 -0300

    Added the script that builds the necessary infrastructure for running the statistics

---
+++ statistics/infra.lua
@@ -0,0 +1,88 @@
+
+local LOG = "infra.log"
+
+local function mkdir(dir_name)
+  assert(type(dir_name) == "string")
+  local MKDIR = string.format("mkdir -p %s >> %s 2>&1", dir_name, LOG)
+  local ret = os.execute(MKDIR)
+  if ret == nil then
+    error("could not create directory: " .. dir_name)
+  end
+end
+
+local function cleandir(dir_name)
+  assert(type(dir_name) == "string")
+  local RM = string.format("rm -rf %s >> %s 2>&1", dir_name, LOG)
+  local ret = os.execute(RM)
+  if ret == nil then
+    error("could not remove directory: " .. dir_name)
+  end
+end
+
+local function rsync(host_name, dir_name)
+  assert(type(host_name) == "string")
+  assert(type(dir_name) == "string")
+  local RSYNC = string.format("rsync -av %s %s >> %s 2>&1", host_name, dir_name, LOG)
+  local ret = os.execute(RSYNC)
+  if ret == nil then
+    error("could not rsync to " .. dir_name)
+  end
+end
+
+local function luarocks_unpack(repo_dir, unpack_dir)
+  assert(type(repo_dir) == "string")
+  assert(type(unpack_dir) == "string")
+  local manifest = repo_dir .. "/manifest"
+  dofile(manifest)
+  assert(type(repository) == "table")
+  for k,v in pairs(repository) do
+    print(string.format("unpacking %s", k))
+    -- it is ok if the package is already unpack
+    os.execute(string.format("cd %s ; luarocks --only-server=%s unpack %s >> %s 2>&1",
+      unpack_dir, repo_dir, k, LOG))
+  end
+end
+
+local function build_luac_list(unpack_dir, luac_list)
+  assert(type(unpack_dir) == "string")
+  assert(type(luac_list) == "string")
+  local file = assert(io.open(luac_list, "w"))
+  local FIND = string.format("find %s -name '*.lua' | sed 's/ /\\\\ /'", unpack_dir)
+  for dotlua in io.popen(FIND):lines() do
+    local ret = os.execute(string.format("luac %s >> %s 2>&1", dotlua, LOG))
+    if ret then
+      file:write(dotlua .. "\n")
+    end
+  end
+  io.close(file)
+  os.remove("luac.out")
+end
+
+if #arg ~= 1 then
+  print("Usage: infra.lua <dir_name>")
+  os.exit(1)
+end
+
+local path = arg[1]
+local repo_dir = path .. "/rocks"
+local unpack_dir = path .. "/unpack"
+local luarocks_host = "luarocks.org::rocks"
+local luac_list = "luac.csv"
+
+print("> removing old log")
+LOG = os.getenv("PWD") .. "/" .. LOG
+os.remove(LOG)
+print(string.format("> creating rocks directory: %s", repo_dir))
+mkdir(repo_dir)
+print(string.format("> rsynching from %s to %s ...", luarocks_host, repo_dir))
+rsync(luarocks_host, repo_dir)
+print(string.format("> preparing unpack directory: %s", unpack_dir))
+cleandir(unpack_dir)
+mkdir(unpack_dir)
+print(string.format("> unpacking rocks from %s to %s", repo_dir, unpack_dir))
+luarocks_unpack(repo_dir, unpack_dir)
+print(string.format("> bulding luac listing to %s", luac_list))
+build_luac_list(unpack_dir, luac_list)
+
+os.exit(0)
+
