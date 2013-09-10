f652b8d741f57dd6fec91abf26fdf12cbda6a19a
Andre Murbach Maidl <andremm@gmail.com>
2013-09-10 15:17:14 -0300

    Adding rules for constant, base, any, and union types in the prototype

--- typedlua/Type.hs
+++ typedlua/Type.hs
@@ -28,11 +28,73 @@ data SndClassType = TFirstClass Type
 
 subtype :: Type -> Type -> Bool
 subtype t1 t2 =
-  subtype_top t1 t2
+  subtype_top t1 t2 ||
+  subtype_any t1 t2 ||
+  subtype_constants t1 t2 ||
+  subtype_base t1 t2 ||
+  subtype_union t1 t2
 
 subtype_top :: Type -> Type -> Bool
 subtype_top t1 t2 =
   case t2 of TObject -> True
              _ -> False
 
+subtype_any :: Type -> Type -> Bool
+subtype_any t1 t2 =
+  case t1 of
+    TAny -> case t2 of
+              TAny -> True
+              _ -> False
+    _ -> False
+
+subtype_constants :: Type -> Type -> Bool
+subtype_constants t1 t2 =
+  case t1 of
+    TConstant CNil -> case t2 of
+                        TConstant CNil -> True
+                        _ -> False
+    TConstant CFalse -> case t2 of
+                          TConstant CFalse -> True
+                          TBase BBoolean -> True
+                          _ -> False
+    TConstant CTrue -> case t2 of
+                         TConstant CTrue -> True
+                         TBase BBoolean -> True
+                         _ -> False
+    TConstant CDouble -> case t2 of
+                           TConstant CDouble -> True
+                           TBase BNumber -> True
+                           _ -> False
+    TConstant CInteger -> case t2 of
+                            TConstant CInteger -> True
+                            TConstant CDouble -> True
+                            TBase BNumber -> True
+                            _ -> False
+    TConstant CString -> case t2 of
+                           TConstant CString -> True
+                           TBase BString -> True
+                           _ -> False
+    _ -> False
+
+subtype_base :: Type -> Type -> Bool
+subtype_base t1 t2 =
+  case t1 of
+    TBase BBoolean -> case t2 of
+                        TBase BBoolean -> True
+                        _ -> False
+    TBase BNumber -> case t2 of
+                       TBase BNumber -> True
+                       _ -> False
+    TBase BString -> case t2 of
+                       TBase BString -> True
+                       _ -> False
+    _ -> False
+
+subtype_union :: Type -> Type -> Bool
+subtype_union t1 t2 =
+  case t1 of
+    TUnion t1' t1'' -> (subtype t1' t2) && (subtype t1'' t2)
+    t1 -> case t2 of
+            TUnion t2' t2'' -> (subtype t1 t2') || (subtype t1 t2'')
+            _ -> False
 
