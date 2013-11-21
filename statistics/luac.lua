dd6bd46fc915e89a003b5882b6f7649adb8efebb
Andre Murbach Maidl <andremm@gmail.com>
2013-11-21 20:39:21 -0200

    Logging all files and files that luac compiles in separated log files

--- statistics/luac.lua
+++ statistics/luac.lua
@@ -1,7 +1,10 @@
 local FIND = "find /Users/andre/tmp/luarocks_repo/unpack/ -name '*.lua'"
+os.remove("total.csv")
+os.remove("compile.csv")
 for dotlua in io.popen(FIND):lines() do
+  os.execute(string.format("echo %s >> total.csv", dotlua))
   local ret = os.execute(string.format("luac '%s' >> luac.log 2>&1", dotlua))
   if ret then
-    print(dotlua)
+    os.execute(string.format("echo %s >> compile.csv", dotlua))
   end
 end
