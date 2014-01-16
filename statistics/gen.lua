078ca5bca4bb6ec70aeaf50b42ee9acfeb5a7401
Andre Murbach Maidl <andremm@gmail.com>
2014-01-16 16:14:35 -0200

    Removing the CSV generation

--- statistics/gen.lua
+++ statistics/gen.lua
@@ -2,32 +2,17 @@
 local statistics = require "statistics"
 
 local function help ()
-  print("Usage: gen.lua <opt> <filename>")
-  print("--csv", "prints in csv style")
-  print("--help", "prints this message")
-  print("--log", "prints in the old style log")
+  print("Usage: gen.lua <filename>")
 end
 
-if #arg ~= 2 then
+if #arg ~= 1 then
   help()
   os.exit(1)
 end
 
-local opt = arg[1]
-local filename = arg[2]
-
-if opt ~= "--csv" and opt ~= "--log" then
-  help()
-  os.exit(1)
-end
+local filename = arg[1]
 
 local result = statistics.generate(filename)
-
-if opt == "--csv" then
-  statistics.print_header()
-  statistics.print_result(filename, result)
-elseif opt == "--log" then
-  statistics.log_result(filename, result)
-end
+statistics.log_result(filename, result)
 
 os.exit(0)
