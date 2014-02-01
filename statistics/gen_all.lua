28636736c9c5ab1d0136b6e6635b3d743568555c
Andre Murbach Maidl <andremm@gmail.com>
2014-02-01 17:40:54 -0200

    Fixing infra script to generate all the necessary files (again)

--- statistics/gen_all.lua
+++ statistics/gen_all.lua
@@ -3,7 +3,7 @@ local statistics = require "statistics"
 
 local merge = statistics.init_merge()
 
-for i in io.lines("luac.csv") do
+for i in io.lines("database.csv") do
   local result = statistics.generate(i)
   statistics.log_result(i, result)
   statistics.merge(result, merge)
