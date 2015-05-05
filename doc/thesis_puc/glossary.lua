72b88c092fb1df27e7ee6b2943a8c4225e4bdf3a
Andre Murbach Maidl <andremm@gmail.com>
2015-05-05 20:22:33 -0300

    Removing and adding some glossary terms

--- doc/thesis_puc/glossary.lua
+++ doc/thesis_puc/glossary.lua
@@ -1,21 +1,14 @@
 
 local terms = {
 -- A
-  "abstract syntax",
+  "algebraic data type",
   "associative array",
-  "application language",
-  "arity mismatch",
   "assertion",
   "axiom",
 -- B
-  "blame annotation",
-  "blame calculus",
   "bottom type",
-  "brand",
-  "bug",
 -- C
   "coercion",
-  "concrete syntax",
   "consistency",
   "consistent-subtyping",
   "contract",
@@ -23,8 +16,6 @@ local terms = {
   "covariant",
 -- D
   "deduction system",
-  "dependent type",
-  "deployment",
   "depth subtyping",
   "downcast",
   "dynamic type",
@@ -33,10 +24,7 @@ local terms = {
 -- E
   "effect system",
 -- F
-  "first-class function",
-  "first-level type",
   "flow typing",
-  "format string",
   "free assigned variable",
 -- G
   "gradual type system",
@@ -44,17 +32,12 @@ local terms = {
   "global type inference",
 -- H
   "higher-order function",
-  "hybrid type checking",
 -- I
   "immutable field",
-  "imperative language",
-  "inheritance",
   "invariant",
 -- J
 -- K
 -- L
-  "lambda calculus",
-  "lexical scope",
   "local type inference",
 -- M
   "metaprogramming",
@@ -64,39 +47,19 @@ local terms = {
   "nominal type system",
   "nilable",
 -- O
-  "objetc-oriented language",
-  "occurence typing",
-  "operator overloading",
   "optional type system",
   "optional typing",
-  "overloading",
 -- P
-  "parametric polymorphism",
-  "partial auxiliary function",
   "pattern matching",
-  "polymorphism",
-  "projection environment",
-  "projection type",
   "prototype object",
 -- Q
-  "quasi-static type system",
 -- R
   "receiver",
   "referenced variable",
-  "refinement type",
-  "reflection",
   "run-time check",
 -- S
-  "scope",
-  "script",
-  "scripting language",
-  "second-level type",
-  "self",
   "self-like delegation",
-  "simpy typed lambda calculus",
-  "single inheritance",
-  "soft typing",
-  "sound",
+  "sound type system",
   "static analysis",
   "static typing",
   "statically typed language",
@@ -108,7 +71,6 @@ local terms = {
   "table constructor",
   "table refinement",
   "top type",
-  "type",
   "type alias",
   "type annotation",
   "type cast",
@@ -117,17 +79,12 @@ local terms = {
   "type environment",
   "type error",
   "type inference",
-  "type parameter",
-  "type predicates",
-  "type refinement",
   "type safety",
   "type system",
   "type tag",
-  "typed lambda calculus",
   "typing relation",
 -- U
-  "unsound",
-  "untyped lambda calculus",
+  "unsound type system",
   "upcast",
   "userdata",
 -- V
