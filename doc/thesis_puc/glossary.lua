e7aeebd8deb73435949f01222aa7b68df166b966
Andre Murbach Maidl <andremm@gmail.com>
2015-05-07 18:43:43 -0300

    Fixing a few more typos

--- doc/thesis_puc/glossary.lua
+++ doc/thesis_puc/glossary.lua
@@ -103,7 +103,6 @@ local def = {
 
 table.sort(terms)
 
-io.write("Should we include Strongtalk, Dart, TypeScript, etc?\n")
 io.write("\\begin{description}\n")
 for k, v in ipairs(terms) do
   io.write(string.format("\\item[%s] %s\n", v, def[v] or "TO DO."))
