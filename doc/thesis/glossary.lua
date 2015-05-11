929f8320394eeac1727570b259cfb68059bc4ff3
Andre Murbach Maidl <andremm@gmail.com>
2015-05-11 11:10:33 -0300

    Defining the glossary terms to be defined

--- doc/thesis/glossary.lua
+++ doc/thesis/glossary.lua
@@ -1,91 +1,58 @@
 
 local terms = {
 -- A
-  "algebraic data type",
-  "associative array",
-  "assertion",
-  "axiom",
 -- B
   "bottom type",
 -- C
+  "closed table type",
   "coercion",
   "consistency",
   "consistent-subtyping",
-  "contract",
   "contravariant",
   "covariant",
 -- D
-  "deduction system",
   "depth subtyping",
-  "downcast",
   "dynamic type",
-  "dynamic typing",
-  "dynamically typed language",
 -- E
-  "effect system",
 -- F
+  "filter type",
+  "fixed table type",
   "flow typing",
   "free assigned variable",
 -- G
   "gradual type system",
   "gradual typing",
-  "global type inference",
 -- H
-  "higher-order function",
 -- I
-  "immutable field",
   "invariant",
 -- J
 -- K
 -- L
-  "local type inference",
 -- M
-  "metaprogramming",
   "metatable",
-  "mutable field",
 -- N
   "nominal type system",
-  "nilable",
 -- O
+  "open table type",
   "optional type system",
-  "optional typing",
 -- P
-  "pattern matching",
+  "projection environment",
+  "projection type",
   "prototype object",
 -- Q
 -- R
-  "receiver",
-  "referenced variable",
-  "run-time check",
 -- S
   "self-like delegation",
   "sound type system",
-  "static analysis",
-  "static typing",
-  "statically typed language",
   "structural type system",
-  "subtype",
-  "subtyping",
-  "supertype",
 -- T
-  "table constructor",
   "table refinement",
   "top type",
-  "type alias",
-  "type annotation",
-  "type cast",
-  "type checker",
-  "type checking",
   "type environment",
-  "type error",
-  "type inference",
-  "type safety",
-  "type system",
   "type tag",
-  "typing relation",
 -- U
+  "unique table type",
   "unsound type system",
-  "upcast",
   "userdata",
 -- V
   "vararg expression",
